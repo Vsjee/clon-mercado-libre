f2fc112ba6277fd302fa48116e780443390e0ccb
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-30 18:20:45 -0500

    fix rating percentage

--- src/app/services/reviews/reviews.service.ts
+++ src/app/services/reviews/reviews.service.ts
@@ -78,10 +78,8 @@ export class ReviewsService {
     let reviewRateGraph: IBarRate[] = MreviewRateGraph
 
     for (let i = 0; i < this.reviews.length; i++) {
-      const currStarsNumber = this.reviews[i].rating
-
-      if (this.valide(currStarsNumber))
-        reviewRateGraph[-currStarsNumber].starsNumber += 1
+      const currStarsNumber = this.reviews[i].rating - 1
+      reviewRateGraph[currStarsNumber].starsNumber += 1
     }
 
     reviewRateGraph.map((item) => {

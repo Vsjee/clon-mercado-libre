986cca7784ce2eac463e4a79f2ce27ef42231325
david hernandez <davidfelipehernandez1@gmail.com>
2023-05-31 22:49:51 -0500

    add prices to the cart

--- src/app/tienda-libre/pages/shopping-cart/shopping-cart.component.ts
+++ src/app/tienda-libre/pages/shopping-cart/shopping-cart.component.ts
@@ -10,12 +10,17 @@ import { AppState, selectCartList } from "src/app/state"
 })
 export class ShoppingCartComponent implements OnInit {
   cartList: IProduct[] = []
+  price: number = 0
+  totalPrice: number = 0
+  shippingPrice: number = 8.5
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.store.select(selectCartList).subscribe((state: IProduct[]) => {
       this.cartList = state
+      state.forEach((item) => (this.price += item.price))
+      this.totalPrice = this.price + this.shippingPrice
     })
   }
 }
